1d8fb330d90be14a420e83b8a99f75fb7bf05af7
Aleksandr Baronov <creigi@yandex.ru>
2023-07-15 18:12:21 +0400

    task done

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,34 @@
 const { test, expect } = require("@playwright/test");
+const { login, password } = require("./testsData/user.js");
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
-
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
-
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
-
-  page.click("text=Бизнес и управление");
-
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://netology.ru");
 });
+
+test("SuccessAutorizationTest", async ({ page }) => {  
+  await page.click("//a[contains(text(), 'Войти')]");
+  await page.locator("//input[@placeholder='Email']").fill(login);
+  await page.locator("//input[@placeholder='Пароль']").fill(password);
+  await page.screenshot({ path: "tests/data/screenshots/SuccessAutorizationTest/screenshot.png" });  
+  
+  await page.locator("//button[@data-testid='login-submit-btn']").click();
+  await page.screenshot({ path: "tests/data/screenshots/SuccessAutorizationTest/screenshot1.png" });
+  
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  await page.waitForSelector("//h2[contains(text(),'Мои курсы и профессии')]");
+  await page.screenshot({ path: "tests/data/screenshots/SuccessAutorizationTest/screenshot2.png" });
+});
+
+test("Unsuccess autorization", async ({ page }) => {
+  await page.click("//a[contains(text(), 'Войти')]");
+  await page.locator("//input[@placeholder='Email']").fill("login@mail.ru");   
+  await page.locator("//input[@placeholder='Пароль']").fill("password");
+  await page.screenshot({ path: "tests/data/screenshots/Unsuccess autorization/screenshot.png" });  
+  
+  await page.locator("//button[@data-testid='login-submit-btn']").click();
+  await page.screenshot({ path: "tests/data/screenshots/Unsuccess autorization/screenshot1.png" });
+
+  await expect(page.locator("[data-testid=login-error-hint]"))
+  .toHaveText("Вы ввели неправильно логин или пароль");
+  await page.screenshot({ path: "tests/data/screenshots/Unsuccess autorization/screenshot2.png" });
+})
